5ce0ce9982917206d606abb3550e2123aba650d7
panosdaveas <panosntaveas@gmail.com>
2021-03-20 11:12:49 +0200

    TestCommit_4

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 import requests
 
 url = 'https://rest.coinapi.io/v1/exchanges'
+headers = {'X-CoinAPI-Key' : 'B284C4BF-9B46-46F4-B377-0E1CA1EEECC7'}
+r = requests.get(url, headers=headers).json()
 
 print(json.dumps(r, indent=4, sort_keys=True))
 # print(r['confirmed'][0])
