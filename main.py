b843142ccd39444138763037b8c61502dd999a90
panosdaveas <panosntaveas@gmail.com>
2021-03-20 15:16:36 +0200

    MongoClient_Setup

--- main.py
+++ main.py
@@ -2,9 +2,33 @@ import json
 
 import requests
 
-url = 'https://rest.coinapi.io/v1/exchanges'
-headers = {'X-CoinAPI-Key' : 'B284C4BF-9B46-46F4-B377-0E1CA1EEECC7'}
-r = requests.get(url, headers=headers).json()
+import pymongo
 
-print(json.dumps(r, indent=4, sort_keys=True))
-# print(r['confirmed'][0])
+from pymongo import MongoClient
+#myclient = MongoClient() #connect to default host & port
+myclient = pymongo.MongoClient("mongodb://localhost:27017")
+mydb = myclient["mydatabase"]
+
+dblist = myclient.list_database_names()
+print(dblist)
+
+if "mydatabase" in dblist:
+    print('The database exists.')
+
+url = 'https://rest.coinapi.io/v1/exchanges'
+headers = {'X-CoinAPI-Key': 'B284C4BF-9B46-46F4-B377-0E1CA1EEECC7'}
+response = requests.get(url, headers=headers).json()
+
+
+mycol = mydb["cryptos"]
+x = mycol.insert_many(response)
+
+print(mydb.list_collection_names())
+query = mycol.find({"exchange_id": {"$regex": "^B"}})
+for y in query:
+    print(y)
+#mycol.drop()
+
+#with open('output.json', 'w') as outfile:
+#    json.dump(response, outfile)
+#
