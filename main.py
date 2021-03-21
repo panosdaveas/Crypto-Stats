f8bcbf6bfcebd97c7fc5d4104216ff97abf3ab59
panosdaveas <panosntaveas@gmail.com>
2021-03-21 11:32:24 +0200

    DumpToJSONfile

--- main.py
+++ main.py
@@ -9,19 +9,21 @@ print(dblist)
 if "mydatabase" in dblist:
     print('The database exists.')
 
-url = 'https://rest.coinapi.io/v1/exchanges'
+url = 'https://rest.coinapi.io/v1/assets'
 headers = {'X-CoinAPI-Key': 'B284C4BF-9B46-46F4-B377-0E1CA1EEECC7'}
 response = requests.get(url, headers=headers).json()
 
-#print(json.dumps(response, indent=4))
+output = open("output.json", "w")
+output.write(json.dumps(response, indent=4))
+print(json.dumps(response, indent=4))
 
 mycollection = mydatabase["cryptos"]
-x = mycollection.insert_many(response)
-myclient.close()
-
-print(mydatabase.list_collection_names())
-for i in range(len(response)):
-    print(response[i]['exchange_id'], response[i]['volume_1hrs_usd'])
+# x = mycollection.insert_many(response)
+# myclient.close()
+#
+# print(mydatabase.list_collection_names())
+# for i in range(len(response)):
+#     print(response[i]['exchange_id'], response[i]['volume_1hrs_usd'])
 
 # query = mycollection.find({"exchange_id": {"$regex": "^BTC"}})
 # mycollection.drop()
