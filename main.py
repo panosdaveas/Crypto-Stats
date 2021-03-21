a5bfde854fae563b3a4c8b620225a0eae7f0d7e6
panosdaveas <panosntaveas@gmail.com>
2021-03-21 15:54:31 +0200

    Plot interations_scatter mode

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 import json
 import requests
 import pymongo
+import matplotlib.pyplot as plt
+import numpy as np
 
 # create and connect to mangoDB
+# pip3 install mongodb-community@4.4
 myclient = pymongo.MongoClient("mongodb://localhost:27017")
 mydatabase = myclient["mydatabase"]
 dblist = myclient.list_database_names()
@@ -20,7 +23,10 @@ response = requests.get(url, headers=headers).json()
 # print(json.dumps(response, indent=4)) # printAll
 
 # populate database's collection
+# currently with asset's name & price in usd
 mycollection = mydatabase["cryptos"]
+listid = []
+listprice =[]
 for key in response:
     if 'price_usd' in key:
         mydict = {'asset_id': key['asset_id'], 'price_usd': key['price_usd']}
@@ -28,8 +34,24 @@ for key in response:
 
 listid = mycollection.distinct("asset_id")
 listprice = mycollection.distinct("price_usd")
-for i in range(len(listid)):
-    print(listid[i], listprice[i])
+# for i in range(len(listid)):
+#    print(listid[i], listprice[i])
+
+# Plot operations
+# pip3 install matplotlib
+xpoints = []
+ypoints = []
+for i in range(10):
+    xpoints.append(listid[i])
+    ypoints.append((listprice[i]))
+    print(xpoints[i], ypoints[i])
+
+x = np.array(xpoints)
+y = np.array(ypoints)
+plt.scatter(x, y)
+plt.xlabel("asset_id")
+plt.ylabel("price_usd")
+plt.show()
 
 # close session
 mycollection.drop()
