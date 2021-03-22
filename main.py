0f8700a0eec813a324a08461bef873abe6521b34
panosdaveas <panosntaveas@gmail.com>
2021-03-22 02:09:36 +0200

    Database_Scheme_Completed

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import requests
 import pymongo
 import matplotlib.pyplot as plt
 import numpy as np
-from pprint import pprint
+from datetime import datetime
+from pprint import pprint  # prettyPrinting
 
 # create and connect to mangoDB
 # pip3 install mongodb-community@4.4
@@ -14,42 +15,31 @@ dblist = myclient.list_database_names()
 print(dblist)
 if "mydatabase" in dblist:
     print('The database exists.')
-# call CoinAPI/v1
-url = 'https://rest.coinapi.io/v1/assets'
+# call CoinAPI/v1 for Bitcoin
+url = 'https://rest.coinapi.io/v1/assets/BTC'
 headers = {'X-CoinAPI-Key': 'B284C4BF-9B46-46F4-B377-0E1CA1EEECC7'}
 response = requests.get(url, headers=headers).json()
 # write to file --optional
-# output = open("output.json", "w")
-# output.write(json.dumps(response, indent=4)) # write to file
-# print(json.dumps(response, indent=4)) # printAll
+output = open("output.json", "w")
+output.write(json.dumps(response, indent=4))  # write to file
+# populate bitcoin_database
+bitcoin = mydatabase[response[0]['asset_id']]
+doc = {"date": datetime.now().strftime('%d-%m-%Y-%H-%M-%S'), "price": response[0]['price_usd']}
+x = bitcoin.insert_one(doc).inserted_id
 
-# populate database's collection
-# currently with asset's name & price in usd
-mycollection = mydatabase["cryptos"]
-myCryptos = {"BTC", "ETH", "DOGE", "ADA", "BNB"}
-for key in response:
-    if 'price_usd' in key:
-        mydict = {"asset_id": key['asset_id'], "price_usd": key['price_usd']}
-        x = mycollection.insert_one(mydict).inserted_id
+listDates = []
+listPrices = []
 
-listName = [mycollection.count_documents({})]
-listPrice = [mycollection.count_documents({})]
-for i in mycollection.find():
-    print('{0} {1}'.format(i['asset_id'], i['price_usd']))
-    listName.append(i['asset_id'])
-    listPrice.append(i['price_usd'])
+for document in bitcoin.find({}, {"_id": False}):
+    listDates.append(document['date'])
+    listPrices.append(document['price'])
 
-query1 = mycollection.find_one({"asset_id": "BTC"})
-query2 = mycollection.find_one({"asset_id": "ETH"})
-for i in mycollection.find():
-    print(i)
 # Plot operations
 # pip3 install matplotlib
-xpoints = np.array(listName[0:10])
-ypoints = np.array(listPrice[0:10])
-plt.scatter(xpoints, ypoints)
+xpoints = np.array(listDates)
+ypoints = np.array(listPrices)
+plt.plot(ypoints)
 plt.show()
-
 # close session
-#mycollection.drop()
-#myclient.close()
+#bitcoin.drop()
+myclient.close()
