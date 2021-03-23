b6108a32441c940d93473967cd70160f9a9127fe
panosdaveas <panosntaveas@gmail.com>
2021-03-23 09:17:36 +0200

    Merged plot_operations branch into main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # imports...
-import json
 import requests
 import pymongo
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
+import os
 
-# create database and connect to mangoDB server
+# create database and connect to mongoDB server
 myclient = pymongo.MongoClient("mongodb://localhost:27017")
 mydatabase = myclient["mydatabase"]
 dblist = myclient.list_database_names()
@@ -14,22 +14,21 @@ print(dblist)
 if "mydatabase" in dblist:
     print('The database exists.')
 
-# call CoinAPI/v1 for Bitcoin
+# call CoinAPI/fetch data for Bitcoin
 url = 'https://rest.coinapi.io/v1/assets/BTC'
 headers = {'X-CoinAPI-Key': 'B284C4BF-9B46-46F4-B377-0E1CA1EEECC7'}
 response = requests.get(url, headers=headers).json()
 
-# populate bitcoin_database
+# populate bitcoin_collection
 bitcoin = mydatabase[response[0]['asset_id']]
 doc = {"date": datetime.now().strftime('%d-%m-%Y, %H:%M:%S'), "price": response[0]['price_usd']}
-x = bitcoin.insert_one(doc).inserted_id
+entry = bitcoin.insert_one(doc).inserted_id
 
 # Plot operations
 listDates = []
 listPrices = []
 
 for document in bitcoin.find({}, {"_id": False}):
-    #formatted_date = document['date'].split(",")
     listDates.append(document['date'])
     listPrices.append(document['price'])
 
@@ -37,7 +36,10 @@ xpoints = np.array(listDates)
 ypoints = np.array(listPrices)
 plt.plot(ypoints, linewidth=0.6)
 plt.grid(axis='y', linestyle='dotted', linewidth=0.5)
-plt.axhline(y=57500, color='r', linestyle='--', linewidth=0.5)
+plt.axhline(y=56590, color='r', linestyle='--', linewidth=0.5)
+x = np.arange(len(ypoints))
+plt.xticks([])
+plt.fill_between(x, ypoints.min(), ypoints, alpha=.1)
 plt.show()
 
 # close session
