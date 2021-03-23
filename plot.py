054d129270cbf15fa4752319d0572a162d48d6a4
panosdaveas <panosntaveas@gmail.com>
2021-03-23 14:03:18 +0200

    Basic_plot_setup

--- plot.py
+++ plot.py
@@ -5,13 +5,12 @@ import numpy as np
 def plot_function(listDates, listPrices):
     xpoints = np.array(listDates)
     ypoints = np.array(listPrices)
-    plt.plot(ypoints, linewidth=0.6, label='current')
+    x = np.arange(len(xpoints))
+    plt.plot(x, ypoints, linewidth=0.6, label='current')
     plt.grid(axis='y', linestyle='dotted', linewidth=0.5)
     plt.axhline(y=54320, color='r', linestyle='--', linewidth=0.5, label='purchased')
-    x = np.arange(len(ypoints))
     plt.xticks([])
     plt.fill_between(x, ypoints.min(), ypoints, alpha=.1)
-    #plt.legend()
     plt.show()
 
 
